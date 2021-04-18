fe172a997a0e18ef65500cbbe3c1326c35914a2c
Hassan Mehmood <hasanmehmood123@yahoo.com>
2021-04-19 02:34:11 +0500

    add some tests for the users post

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -14,3 +14,27 @@ def test_add_user(test_app, test_database):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 201
     assert 'john@test.com was added!' in data['message']
+
+
+def test_invalid_json_request(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        'users',
+        data=json.dumps({}),
+        content_type='application/json'
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
+
+
+def test_user_already_exists(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        'users',
+        data=json.dumps({
+            'username': 'john',
+            'email': 'john@test.com'
+        }),
+        content_type='application/json'
+    )
